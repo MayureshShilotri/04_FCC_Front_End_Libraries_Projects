a653793dd268139947347d2fa098ec384a4007f7
Mayuresh Shilotri <mayuresh.shilotri@gmail.com>
2020-06-29 08:00:47 +0530

    Issues: 1. Does not do 1st time load 2. Works only once 2. Does not refresh

---
+++ Project 1/main.js
@@ -0,0 +1,40 @@
+// DOM is ready
+document.addEventListener("DOMContentLoaded", function() {
+  // DATA
+  const quotesPool = [
+    ["Less is More", "Ludwig Mies Van Der Rohe"],
+    ["God is in the Details", "Ludwig Mies Van Der Rohe"],
+    ["Architecture starts when you carefully put two bricks together. There it begins", "Ludwig Mies Van Der Rohe"],
+    ["Good building come from good people and all problems are solved by good design", "Stephen Gardiner"],
+    ["There are 360 degrees, so why stick to one?", "Zaha Hadid"],
+    ["Form ever follows function", "Louis Sullivan"],
+    ["Recognizing the need is the primary condition of design", "Charles Eames"],
+    ["Architecture is the learned game, correct and magnificent, of forms assembled in the light", "Le Corbusier"],
+    ["An idea is salvation by imagination", "Frank Lloyd Wright"],
+    ["When I'm working on a problem, I never think about beauty. But when I've finished, if the solution is not beautiful I know it's wrong", "Buckminster Fuller"],
+    ["As an architect you design for the present, with an awareness of the past, for a future which is essentially unknown", "Norman Foster"],
+    ["To provide meaningful architecture is not to parody history but to articulate it", "Daniel Libeskind"],
+    ["To create, one must first question everything", "Eileen Gray"],
+    ["One of the great beauties of architecture is that each time, it is like life starting all over again", "Renzo Piano"],
+    ["You’ve got to bumble forward into the unknown", "Frank Gehry"]
+  ];
+
+  // onClick event binding
+  function getNewQuote() {
+    var randomQuoteButton = document.querySelector("#new-quote");
+    var quote = document.getElementById("text");
+    var author = document.getElementById("author");
+    var randomIndex = Math.floor((Math.random() * quotesPool.length) + 1);
+
+    randomQuoteButton.addEventListener("click", function() {
+      quote.innerHTML = quotesPool[randomIndex][0];
+      author.innerHTML = quotesPool[randomIndex][1];
+    });
+  }
+
+  // Get 1ST quote
+  window.onload = function() {
+    getNewQuote();
+  };
+
+});
