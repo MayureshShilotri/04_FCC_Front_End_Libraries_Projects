92cd5eebb4c9177d21c293fef465d98b610ca495
Mayuresh Shilotri <mayuresh.shilotri@gmail.com>
2020-07-03 18:26:53 +0530

    4 out of 8 test cases done Basic Flex in place

--- Project 3/drummachine/src/App.js
+++ Project 3/drummachine/src/App.js
@@ -2,10 +2,19 @@ import './App.css';
 import React from 'react'
 
 class App extends React.Component {
-  render () {
-    return (
-        <h1>Hello World</h1>
-    );
+  render() {
+    return (<div className="container" id="drum-machine">
+      <h1 id = "display">Drum Machine</h1>
+      <div className = "drum-pad" id="Q">Q</div>
+      <div className = "drum-pad" id="W">W</div>
+      <div className = "drum-pad" id="E">E</div>
+      <div className = "drum-pad" id="A">A</div>
+      <div className = "drum-pad" id="S">S</div>
+      <div className = "drum-pad" id="D">D</div>
+      <div className = "drum-pad" id="Z">Z</div>
+      <div className = "drum-pad" id="X">X</div>
+      <div className = "drum-pad" id="C">C</div>
+    </div>);
   }
 }
 
